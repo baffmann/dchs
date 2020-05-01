a4eda4975acab61716646fc9aeec336bed2cd232
baffmann <benjamin.beisel@gmail.com>
2020-05-01 15:22:16 +0200

    added shutdown interface

--- main.go
+++ main.go
@@ -93,7 +93,7 @@ func main() {
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir(webPath)))
 
 	fmt.Println("Starting webserver at port 64760")
-	err = http.ListenAndServe(":64765", r)
+	err = http.ListenAndServe(":64760", r)
 	if err != nil {
 		fmt.Println("Error starting webserver: ", err)
 		os.Exit(1)
